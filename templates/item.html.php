f68ec767fa549f5f862e849d3395849e98c2afc1
Josh Brickner <josh@macinjo.sh>
2023-11-12 10:02:34 -0700

    Styling tweaks

--- templates/item.html.php
+++ templates/item.html.php
@@ -1,4 +1,4 @@
-<div id="item-desc" class="container py-10 content-center">
+<div class="container py-10 content-center">
 
     <h1 class="py-5 text-2xl">
     <a href="/feed?id=<?= $item['feed_id'] ?>"><?= $feed['name'] ?></a>
@@ -7,12 +7,12 @@
     <h1 class="text-xl pb-5"><?= $item['name'] ?></h1>
     <img src="/image?<?= 'item_id='.$item['id'] ?: 'feed_id'.$feed['id'] ?>" class="mx-auto w-3/5 border-solid border-neutral-800 border">
     <audio autoplay controls src="/audio?item_id=<?= $item['id'] ?>" class="w-full p-4 h-13"></audio>
-    <div class="w-full pb-4 pl-5 pr-5">
+    <div id="item-desc" class="w-full pb-4 pl-5 pr-5">
         <p class="pb-4"><?= $item['description'] ?></p>
     </div>
 
 </div>
 <style type="text/css">
-   #item-desc a {color: red;}
+   #item-desc a {color: rgb(253, 230, 138);}
    #item-desc p {padding-bottom: 1em;}
 </style>
