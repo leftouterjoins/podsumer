9f7d49a193710fa723d3d979e2aa49c88466d690
Josh Brickner <josh@macinjo.sh>
2023-11-12 21:38:14 -0700

    RSS Fix

--- templates/rss.xml.php
+++ templates/rss.xml.php
@@ -2,7 +2,7 @@
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:podcast="https://podcastindex.org/namespace/1.0">
   <channel>
     <title><?= $feed['name'] ?></title>
-    <description><?= htmlentities(strip_tags($feed['description']), ENT_XHTML) ?></description>
+    <description><?=  htmlentities(strip_tags(strval($feed['description'])), ENT_QUOTES | ENT_SUBSTITUTE | ENT_XML1, '', false) ?></description>
     <itunes:image href="<?= $host ?>/image?feed_id=<?= $feed['id'] ?>" />
     <language>en-us</language>
     <generator>podsumer</generator>
@@ -13,7 +13,7 @@
     <? foreach($items as $item): ?>
     <item>
       <title><?= $item['name'] ?></title>
-      <description><?= htmlentities(strip_tags($item['description']), ENT_XHTML) ?></description>
+      <description><?= htmlentities(strip_tags(strval($item['description'])), ENT_QUOTES | ENT_SUBSTITUTE | ENT_XML1, '', false) ?></description>
       <pubDate><?= $item['published'] ?></pubDate>
       <enclosure url="<?= $host ?>/media?item_id=<?= $item['id'] ?>" type="audio/mp3" length="<?= $item['size'] ?>"/>
       <link><?= $host ?>/item?item_id=<?= $item['id'] ?></link>
