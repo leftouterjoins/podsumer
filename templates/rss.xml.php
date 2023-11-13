52dad763f8943a385b131b6879f24746ee04518e
Josh Brickner <josh@macinjo.sh>
2023-11-12 21:28:13 -0700

    More RSS escaping

--- templates/rss.xml.php
+++ templates/rss.xml.php
@@ -2,7 +2,7 @@
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:podcast="https://podcastindex.org/namespace/1.0">
   <channel>
     <title><?= $feed['name'] ?></title>
-    <description><?= $feed['description'] ?></description>
+    <description><?= htmlentities(strip_tags($feed['description'])) ?></description>
     <itunes:image href="<?= $host ?>/image?feed_id=<?= $feed['id'] ?>" />
     <language>en-us</language>
     <generator>podsumer</generator>
@@ -13,7 +13,7 @@
     <? foreach($items as $item): ?>
     <item>
       <title><?= $item['name'] ?></title>
-      <description><?= strip_tags($item['description']) ?></description>
+      <description><?= htmlentities(strip_tags($item['description'])) ?></description>
       <pubDate><?= $item['published'] ?></pubDate>
       <enclosure url="<?= $host ?>/media?item_id=<?= $item['id'] ?>" type="audio/mp3" length="<?= $item['size'] ?>"/>
       <link><?= $host ?>/item?item_id=<?= $item['id'] ?></link>
