5bca3f4c15a5b7f7461ed344bd30322fb7e4416b
Josh Brickner <josh@macinjo.sh>
2023-11-18 08:43:20 -0700

    Updates to Files, Episode Page Layout, and Configs
    - Files are no longer required to be stored under the project root.
    - File records in the DB are now immutable.
    - Fixed layout issue with <audio> tag on episode page
    - Updated config defaults to reflect above changes to file storage

--- templates/item.html.php
+++ templates/item.html.php
@@ -1,19 +1,20 @@
-<div class="container py-10 content-center">
+<div class="container py-10 content-center clear">
 
     <h1 class="py-5 text-2xl">
-    <a href="/feed?id=<?= $item['feed_id'] ?>"><?= $feed['name'] ?></a>
+        <a href="/feed?id=<?= $item['feed_id'] ?>"><?= $feed['name'] ?></a>
     </h1>
 
     <h1 class="text-xl pb-5"><?= $item['name'] ?></h1>
-    <img src="/image?<?= 'item_id='.$item['id'] ?: 'feed_id'.$feed['id'] ?>" class="mx-auto w-3/5 border-solid border-neutral-800 border">
-    <audio autoplay controls src="/audio?item_id=<?= $item['id'] ?>" class="w-full p-4 h-13"></audio>
-    <div id="item-desc" class="w-full pb-4 pl-5 pr-5">
-        <p class="pb-4"><?= $item['description'] ?></p>
-    </div>
+
+    <p><img src="/image?<?= 'item_id='.$item['id'] ?: 'feed_id'.$feed['id'] ?>" class="mx-auto w-3/5 border-solid border-neutral-800 border"></p>
+
+    <p><audio autoplay controls src="/audio?item_id=<?= $item['id'] ?>" class="w-full m-4 h-13"></audio></p>
+
+    <p><?= $item['description'] ?></p>
 
 </div>
+
 <style type="text/css">
    #item-desc a {color: rgb(253, 230, 138);}
    #item-desc p {padding-bottom: 1em;}
 </style>
-
