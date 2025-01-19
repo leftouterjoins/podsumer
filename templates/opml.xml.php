b31d870313205f43b9775b7cb35ae588d0fa1253
Josh Brickner <15388+leftouterjoins@users.noreply.github.com>
2025-01-19 10:45:36 -0700

    More updates to OPML format

--- templates/opml.xml.php
+++ templates/opml.xml.php
@@ -1,12 +1,11 @@
 <?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
-<opml version="2.0">
+<opml version="1.0">
     <head>
         <title>Podsumer Feeds</title>
-        <dateCreated><?= date('r') ?></dateCreated>
     </head>
     <body>
         <? foreach ($feeds as $feed): ?>
-        <outline text="<?= htmlspecialchars($feed['name']) ?>" xmlUrl="<?= $host ?>/rss?feed_id=<?= $feed['id'];?>"  htmlUrl="<?= $host ?>/feed?id=<?= $feed['id'];?>" type="rss"></outline>
+        <outline title="<?= htmlspecialchars($feed['name']) ?>"  text="<?= htmlspecialchars($feed['name']) ?>" xmlUrl="<?= $host ?>/rss?feed_id=<?= $feed['id'];?>"  htmlUrl="<?= $host ?>/feed?id=<?= $feed['id'];?>" type="rss" />
         <? endforeach ?>
     </body>
 </opml>
