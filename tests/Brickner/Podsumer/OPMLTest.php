5d49bd3574c6c418e01f5f89152bdd2ddc387cda
Josh Brickner <josh@macinjo.sh>
2025-06-16 16:55:11 -0600

    localized test

--- tests/Brickner/Podsumer/OPMLTest.php
+++ tests/Brickner/Podsumer/OPMLTest.php
@@ -8,7 +8,7 @@ final class OPMLTest extends TestCase
 {
     public function testParse()
     {
-        $opml_data = file_get_contents('http://hosting.opml.org/dave/spec/states.opml');
+        $opml_data = file_get_contents(__DIR__ . '/../../opml-sample.opml');
         $tmp = tempnam("/tmp", "opml-test");
         file_put_contents($tmp, $opml_data);
         $opml = OPML::parse(['tmp_name' => $tmp]);
